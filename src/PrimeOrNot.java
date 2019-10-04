3ddff63dbdfe6977357e2e2991c0bfff5447ec80
League Student <league@iMac-11.attlocal.net>
2019-10-03 18:28:56 -0700

    GoofyNames, PrimeOrNot IP :smile:

---
+++ src/PrimeOrNot.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+		String numie = JOptionPane.showInputDialog("Enter a number below.");
+		Integer.parseInt(numie);
+	}
+}
