b00411bc1accc4c4845a4165e60ab098849d404b
League Student <league@iMac-23.attlocal.net>
2019-10-12 11:28:56 -0700

    PrimeOrNot, GuessingPi, MissingMoustaches, Fibonacci IP :smile:

--- src/PrimeOrNot.java
+++ src/PrimeOrNot.java
@@ -2,7 +2,16 @@ import javax.swing.JOptionPane;
 
 public class PrimeOrNot {
 	public static void main(String[] args) {
-		String numie = JOptionPane.showInputDialog("Enter a number below.");
-		Integer.parseInt(numie);
+		for (int i = 0; i < 4; i++) {
+			String numie = JOptionPane.showInputDialog("Enter a number below.");
+			int numi = Integer.parseInt(numie);
+			for (int j = 2; j < numi; j++) {
+				if (numi % j == 0) {
+					JOptionPane.showMessageDialog(null, "Your number is not prime.");
+					System.exit(0);
+				}
+			}
+			JOptionPane.showMessageDialog(null, "Your number is prime.");
+		}
 	}
 }
